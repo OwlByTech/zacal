653d4b84d376591c5387d3a46065586e166fcf68
CamiloArias12 <ariaspira13@gmail.com>
2024-04-02 19:11:03 -0500

    change style

--- packages/web/src/modules/common/components/arrow/index.tsx
+++ packages/web/src/modules/common/components/arrow/index.tsx
@@ -1,13 +1,6 @@
-import { Suspense } from "react"
-
-import { listRegions } from "@lib/data"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import CartButton from "@modules/layout/components/cart-button"
-import SideMenu from "@modules/layout/components/side-menu"
-
 type Props = { onClick: any; direction?: boolean }
 
-export default async function Arrow({ onClick, direction }: Props) {
+export default function Arrow({ onClick, direction }: Props) {
   return (
     <button
       className={` border border-black bg-white p-1 ${
