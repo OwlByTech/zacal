05bf03a3594f8ec81a91715372a670310fddc256
CamiloArias12 <ariaspira13@gmail.com>
2024-04-04 07:26:50 -0500

    add size iamge

--- packages/web/src/modules/products/components/image-gallery/index.tsx
+++ packages/web/src/modules/products/components/image-gallery/index.tsx
@@ -9,21 +9,6 @@ type ImageGalleryProps = {
 const ImageGallery = ({ images }: ImageGalleryProps) => {
   return (
     <div className="flex flex-col gap-2  ">
-      <div className="flex flex-row overflow-x-auto h-28 gap-2 sticky top-36 bg-white pb-2 border-b-2 border-gray-900">
-        {images.map((image, index) => {
-          return (
-            <img
-              src={image.url}
-              className="h-full object-fill rounded-none inset-0 hover:scale-110"
-              alt={`Product image ${index + 1}`}
-              sizes="(max-width: 576px) 80px, (max-width: 768px) 100px, (max-width: 992px) 150px, 100px"
-              style={{
-                objectFit: "fill",
-              }}
-            />
-          )
-        })}
-      </div>
       <div className="flex flex-col overflow-y-auto  gap-y-4">
         {images.map((image, index) => {
           return (
