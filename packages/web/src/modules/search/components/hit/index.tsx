6440f22439c3a802cb643c57fa52093cd6322b42
CamiloArias12 <ariaspira13@gmail.com>
2024-04-10 18:00:31 -0500

    add hit search

--- packages/web/src/modules/search/components/hit/index.tsx
+++ packages/web/src/modules/search/components/hit/index.tsx
@@ -24,14 +24,14 @@ const Hit = ({ hit }: HitProps) => {
     <LocalizedClientLink href={`/products/${hit.handle}`}>
       <Container
         key={hit.id}
-        className="flex sm:flex-col gap-2 w-full p-4 shadow-elevation-card-rest hover:shadow-elevation-card-hover items-center sm:justify-center"
+        className="flex sm:flex-row gap-2 w-full p-4 shadow-elevation-card-rest hover:shadow-elevation-card-hover items-center sm:justify-center"
       >
         <Thumbnail
           thumbnail={hit.thumbnail}
           size="square"
-          className="group h-12 w-12 sm:h-full sm:w-full"
+          className="group h-4 w-4 sm:h-8 sm:w-8"
         />
-        <div className="flex flex-col justify-between group">
+        <div className="flex flex-col w-4/5 justify-between group">
           <div className="flex flex-col">
             <Text className="text-ui-fg-subtle">{hit.title}</Text>
           </div>
