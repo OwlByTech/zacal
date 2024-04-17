b10e26228e7fc7669c3f9f58b5c70b3264a0da8b
CamiloArias12 <ariaspira13@gmail.com>
2024-04-17 17:22:50 -0500

    add filter

--- packages/web/src/modules/store/components/refinement-list/index.tsx
+++ packages/web/src/modules/store/components/refinement-list/index.tsx
@@ -4,6 +4,9 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useCallback } from "react"
 
 import SortProducts, { SortOptions } from "./sort-products"
+import ColorProducts from "./color-products"
+import MaterialProducts from "./material-products"
+import SizeProducts from "./size-products"
 
 type RefinementListProps = {
   sortBy: SortOptions
@@ -31,7 +34,27 @@ const RefinementList = ({ sortBy }: RefinementListProps) => {
   }
 
   return (
-    <div className="flex small:flex-col gap-12 py-4 mb-8 small:px-0 pl-6 small:min-w-[250px] small:ml-[1.675rem]">
+    <div className="flex small:flex-col gap-12 py-4 mb-8 small:px-0 small:min-w-[450px] text-black ">
+      <div className="flex flex-col gap-2">
+        <p className="font-bold pb-1 border-b border-black">Material</p>
+
+        <div className="px-2">
+          <MaterialProducts setQueryParams={setQueryParams} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="font-bold pb-1 border-b border-black">Color</p>
+        <div className="px-2">
+          <ColorProducts setQueryParams={setQueryParams} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="font-bold pb-1 border-b border-black">Talla</p>
+        <div className="px-2">
+          <SizeProducts setQueryParams={setQueryParams} />
+        </div>
+      </div>
+
       <SortProducts sortBy={sortBy} setQueryParams={setQueryParams} />
     </div>
   )
