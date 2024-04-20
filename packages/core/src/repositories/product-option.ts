c4ffe5d167b8c4f5d940e031d9864a288aeac349
CamiloArias12 <ariaspira13@gmail.com>
2024-04-20 18:05:52 -0500

    add sevice filter

--- packages/core/src/repositories/product-option.ts
+++
@@ -1,6 +0,0 @@
-import { dataSource } from "@medusajs/medusa/dist/loaders/database";
-import { ProductOption } from "@medusajs/medusa"; "@medusajs/medusa/dist/models/product-option";
-
-const ProductOptionRepository = dataSource.getRepository(ProductOption);
-
-export default ProductOptionRepository;
