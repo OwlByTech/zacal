bd202c9e4dab434f85a775fc951dd04a09cc8b82
CamiloArias12 <ariaspira13@gmail.com>
2024-04-21 11:39:23 -0500

    delete colecctions

--- packages/web/src/app/[countryCode]/(main)/page.tsx
+++ packages/web/src/app/[countryCode]/(main)/page.tsx
@@ -22,56 +22,14 @@ export const metadata: Metadata = {
     "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
 }
 
-const getCollectionsWithProducts = cache(
-  async (
-    countryCode: string
-  ): Promise<ProductCollectionWithPreviews[] | null> => {
-    const { collections } = await getCollectionsList(0, 3)
-
-    if (!collections) {
-      return null
-    }
-
-    const collectionIds = collections.map((collection) => collection.id)
-
-    await Promise.all(
-      collectionIds.map((id) =>
-        getProductsList({
-          queryParams: { collection_id: [id] },
-          countryCode,
-        })
-      )
-    ).then((responses) =>
-      responses.forEach(({ response, queryParams }) => {
-        let collection
-
-        if (collections) {
-          collection = collections.find(
-            (collection) => collection.id === queryParams?.collection_id?.[0]
-          )
-        }
-
-        if (!collection) {
-          return
-        }
-
-        collection.products = response.products as unknown as Product[]
-      })
-    )
-
-    return collections as unknown as ProductCollectionWithPreviews[]
-  }
-)
-
 export default async function Home({
   params: { countryCode },
 }: {
   params: { countryCode: string }
 }) {
-  const collections = await getCollectionsWithProducts(countryCode)
   const region = await getRegion(countryCode)
   const customer = await getCustomer()
-  if (!collections || !region) {
+  if (!region) {
     return null
   }
 
@@ -89,11 +47,6 @@ export default async function Home({
         <div className="flex flex-col gap-5 sm:gap-20">
           <OutletCategory region={region} />
         </div>
-      </div>
-      <div className="py-12">
-        <ul className="flex flex-col gap-x-6">
-          <FeaturedProducts collections={collections} region={region} />
-        </ul>
       </div>
     </>
   )
