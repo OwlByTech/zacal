190ae4ca4294e7a9d3f4fbb7421fd7d5880768cc
CamiloArias12 <ariaspira13@gmail.com>
2024-05-01 13:39:33 +0000

    fix scroll mobile

--- packages/web/src/modules/store/templates/paginated-products.tsx
+++ packages/web/src/modules/store/templates/paginated-products.tsx
@@ -1,30 +1,7 @@
-import {
-  getProductsListFilter,
-  getProductsListWithSort,
-  getRegion,
-} from "@lib/data"
-import ProductPreview from "@modules/products/components/product-preview"
-import { Pagination } from "@modules/store/components/pagination"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
-import { method } from "lodash"
 import ListProducts from "../components/list-products/list-products"
 import { Suspense } from "react"
 import SkeletonProductGrid from "@modules/skeletons/templates/skeleton-product-grid"
-
-const PRODUCT_LIMIT = 20
-
-type PaginatedProductsParams = {
-  limit: number
-  collection_id?: string[]
-  category?: string[]
-  color?: string[]
-  material?: string[]
-  size?: string[]
-  id?: string[]
-  minPrice?: number
-  maxPrice?: number
-  sort?: SortOptions
-}
 
 export default async function PaginatedProducts({
   sortBy,
